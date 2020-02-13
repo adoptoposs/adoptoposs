feedc6ef40b6e0c3427666a28ef65c696393517d
Paul Götze <paul.christoph.goetze@gmail.com>
2020-02-13 12:55:05 +0100

    Add RequireLogin plug to handle route access

---
+++ lib/adoptoposs_web/plugs/require_login.ex
@@ -0,0 +1,30 @@
+defmodule AdoptopossWeb.Plugs.RequireLogin do
+  @moduledoc """
+  Plug for redirecting the request when the user is not logged in.
+  """
+
+  import Plug.Conn
+  import Phoenix.Controller, only: [put_flash: 3, redirect: 2]
+  alias AdoptopossWeb.Router.Helpers, as: Routes
+
+  alias Adoptoposs.Accounts.User
+
+  def init(opts), do: opts
+
+  @doc """
+  Pass through if user is logged in.
+  """
+  def call(%Plug.Conn{assigns: %{current_user: %User{}}} = conn, _opts) do
+    conn
+  end
+
+  @doc """
+  Redirect to login if user is logged out.
+  """
+  def call(conn, _opts) do
+    conn
+    |> put_flash(:error, "You need to log in to visit this page.")
+    |> redirect(to: Routes.page_path(conn, :index))
+    |> halt()
+  end
+end
---
+++ test/adoptoposs_web/plugs/require_login_test.exs
@@ -0,0 +1,41 @@
+defmodule AdoptoppossWeb.Plugs.RequireLoginTest do
+  use AdoptopossWeb.ConnCase
+
+  alias Adoptoposs.Accounts.User
+  alias AdoptopossWeb.Plugs.RequireLogin
+
+  setup(%{conn: conn}) do
+    conn =
+      conn
+      |> init_test_session(%{})
+      |> fetch_flash()
+
+    {:ok, %{conn: conn}}
+  end
+
+  test "user is redirected if current_user is not assigned", %{conn: conn} do
+    conn = require_login(conn)
+
+    assert redirected_to(conn) == Routes.page_path(conn, :index)
+    assert get_flash(conn, :error) =~ "You need to log in"
+    assert %{halted: true} = conn
+  end
+
+  test "user passes through if current_user is assigned", %{conn: conn} do
+    conn =
+      conn
+      |> authenticate()
+      |> require_login()
+
+    assert conn.status != 302
+    assert %{halted: false} = conn
+  end
+
+  defp authenticate(conn) do
+    conn |> assign(:current_user, %User{})
+  end
+
+  defp require_login(conn) do
+    conn |> RequireLogin.call(%{})
+  end
+end
