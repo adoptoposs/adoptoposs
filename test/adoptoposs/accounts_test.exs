623842909cdfd0cebd0ecc95a14f03999561f637
Paul Götze <paul.christoph.goetze@gmail.com>
2020-02-13 18:23:22 +0100

    Bring back top-level describe macro for accounts tests
    Just for the sake of readability and consistency with generated tests.

--- test/adoptoposs/accounts_test.exs
+++ test/adoptoposs/accounts_test.exs
@@ -6,36 +6,34 @@ defmodule Adoptoposs.AccountsTest do
   alias Adoptoposs.Accounts
   alias Adoptoposs.Accounts.User
 
-  test "get_user!/1 returns the user with given id" do
-    user = insert(:user)
-    assert Accounts.get_user!(user.id) == user
-  end
+  describe "accounts" do
+    test "get_user!/1 returns the user with given id" do
+      user = insert(:user)
+      assert Accounts.get_user!(user.id) == user
+    end
 
-  describe "get_user_by_auth/1" do
-    test "returns the user with the given uid and provider" do
+    test "get_user_by_auth/1 returns the user with the given uid and provider" do
       user = insert(:user)
       auth = build(:auth, uid: user.uid, provider: user.provider)
       assert Accounts.get_user_by_auth(auth) == user
     end
 
-    test "returns nil if there is no user matching the given auth" do
+    test "get_user_by_auth/1 returns nil if there is no user matching the given auth" do
       auth = build(:auth)
       assert Accounts.get_user_by_auth(auth) == nil
     end
 
-    test "returns nil for auths without a uid" do
+    test "get_user_by_auth/1 returns nil for auths without a uid" do
       auth = build(:auth, uid: nil)
       assert Accounts.get_user_by_auth(auth) == nil
     end
 
-    test "returns nil for auths without a provider" do
+    test "get_user_by_auth/1 returns nil for auths without a provider" do
       auth = build(:auth, provider: nil)
       assert Accounts.get_user_by_auth(auth) == nil
     end
-  end
 
-  describe "upsert_user!/2" do
-    test "for missing user creates the user" do
+    test "upsert_user!/2 for missing user creates the user" do
       auth = build(:auth)
 
       assert {:ok, %User{} = user} = Accounts.upsert_user(auth)
@@ -51,7 +49,7 @@ defmodule Adoptoposs.AccountsTest do
              }
     end
 
-    test "for available user updates the user" do
+    test "upsert_user!/2 for available user updates the user" do
       user = insert(:user)
       auth = build(:auth, uid: user.uid, provider: user.provider)
 
@@ -69,7 +67,7 @@ defmodule Adoptoposs.AccountsTest do
              }
     end
 
-    test "with valid data creates a user" do
+    test "upsert_user!/2 with valid data creates a user" do
       attrs = build(:user) |> Map.from_struct()
       assert {:ok, %User{} = user} = Accounts.create_user(attrs)
 
@@ -83,15 +81,13 @@ defmodule Adoptoposs.AccountsTest do
                profile_url: attrs.profile_url
              }
     end
-  end
 
-  test "create_user/1 with invalid data returns error changeset" do
-    attrs = build(:user, uid: nil, provider: nil) |> Map.from_struct()
-    assert {:error, %Ecto.Changeset{}} = Accounts.create_user(attrs)
-  end
+    test "create_user/1 with invalid data returns error changeset" do
+      attrs = build(:user, uid: nil, provider: nil) |> Map.from_struct()
+      assert {:error, %Ecto.Changeset{}} = Accounts.create_user(attrs)
+    end
 
-  describe "update_user/2" do
-    test "with valid data updates the user" do
+    test "update_user/2 with valid data updates the user" do
       user = insert(:user)
       attrs = build(:user) |> Map.from_struct()
 
@@ -109,22 +105,22 @@ defmodule Adoptoposs.AccountsTest do
              }
     end
 
-    test "with invalid data returns error changeset" do
+    test "update_user/2 with invalid data returns error changeset" do
       user = insert(:user)
       attrs = build(:user, uid: nil, provider: nil) |> Map.from_struct()
       assert {:error, %Ecto.Changeset{}} = Accounts.update_user(user, attrs)
       assert user == Accounts.get_user!(user.id)
     end
-  end
 
-  test "delete_user/1 deletes the user" do
-    user = insert(:user)
-    assert {:ok, %User{}} = Accounts.delete_user(user)
-    assert_raise Ecto.NoResultsError, fn -> Accounts.get_user!(user.id) end
-  end
+    test "delete_user/1 deletes the user" do
+      user = insert(:user)
+      assert {:ok, %User{}} = Accounts.delete_user(user)
+      assert_raise Ecto.NoResultsError, fn -> Accounts.get_user!(user.id) end
+    end
 
-  test "change_user/1 returns a user changeset" do
-    user = insert(:user)
-    assert %Ecto.Changeset{} = Accounts.change_user(user)
+    test "change_user/1 returns a user changeset" do
+      user = insert(:user)
+      assert %Ecto.Changeset{} = Accounts.change_user(user)
+    end
   end
 end
