5b388fb8d4bf96bf33c23547a8df0eec798bb923
Paul Götze <paul.christoph.goetze@gmail.com>
2020-02-20 11:54:36 +0100

    Add file-loader to handle custom font and svg loading

--- assets/webpack.config.js
+++ assets/webpack.config.js
@@ -45,6 +45,10 @@ module.exports = (env, options) => ({
             },
           },
         ]
+      },
+      {
+        test   : /\.(ttf|eot|svg|woff(2)?)(\?[a-z0-9=&.]+)?$/,
+        loader : 'file-loader'
       }
     ]
   },
