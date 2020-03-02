218836184d6c5fa7f738cef0f094f9a810cb330c
Paul Götze <paul.christoph.goetze@gmail.com>
2020-03-02 20:51:00 +0100

    Add dashboard page for logged in users

--- lib/adoptoposs_web/live/page_live.ex
+++ lib/adoptoposs_web/live/page_live.ex
@@ -1,20 +1,39 @@
 defmodule AdoptopossWeb.PageLive do
   use AdoptopossWeb, :live_view
 
-  alias Adoptoposs.Dashboard
+  alias Adoptoposs.{Dashboard, Tags}
   alias AdoptopossWeb.PageView
 
-  def render(assigns) do
+  @doc """
+  Renders the logged out landing page template.
+  """
+  def render(%{user_id: nil} = assigns) do
     Phoenix.View.render(PageView, "index.html", assigns)
   end
 
-  def mount(_params, session, socket) do
-    projects = Dashboard.list_projects(limit: 6)
+  @doc """
+  Renders the logged-in user's dashbard template.
+  """
+  def render(assigns) do
+    Phoenix.View.render(PageView, "dashboard.html", assigns)
+  end
 
+  def mount(_params, session, socket) do
     {:ok,
      socket
      |> assign_user(session)
-     |> assign(projects: projects)}
+     |> put_assigns(session)}
+  end
+
+  defp put_assigns(socket, %{"current_user" => user}) do
+    assign(socket,
+      projects: Dashboard.list_projects(limit: 6),
+      tags: Tags.list_user_tag_subscriptions(user) |> Enum.map(& &1.tag)
+    )
+  end
+
+  defp put_assigns(socket, _) do
+    assign(socket, projects: Dashboard.list_projects(limit: 6))
   end
 
   defp assign_user(socket, %{"current_user" => user}) do
--- test/adoptoposs_web/live/page_live_test.exs
+++ test/adoptoposs_web/live/page_live_test.exs
@@ -1,14 +1,30 @@
 defmodule AdoptopossWeb.PageLiveTest do
   use AdoptopossWeb.ConnCase
 
-  test "GET /", %{conn: conn} do
-    conn = get(conn, "/")
-    assert html_response(conn, 200) =~ "Adoptoposs"
+  import Phoenix.LiveViewTest
+  import Adoptoposs.Factory
+
+  alias AdoptopossWeb.PageLive
+
+  test "GET / shows the landing page for not logged in users", %{conn: conn} do
+    conn = get(conn, Routes.live_path(conn, PageLive))
+
+    assert html_response(conn, 200) =~ "Adoptoposs."
+    refute html_response(conn, 200) =~ "Projects For You"
+    {:ok, _view, _html} = live(conn)
   end
 
-  test "does not require authentication on GET /", %{conn: conn} do
-    conn = get(conn, Routes.live_path(conn, AdoptopossWeb.PageLive))
-    assert html_response(conn, 200)
-    refute conn.halted
+  test "GET / shows the user dashboard for logged in users", %{conn: conn} do
+    user = insert(:user)
+
+    conn =
+      conn
+      |> init_test_session(%{current_user: user})
+      |> put_req_header("content-type", "html")
+      |> get(Routes.live_path(conn, PageLive))
+
+    assert html_response(conn, 200) =~ "Projects For You"
+    refute html_response(conn, 200) =~ "Adoptoposs."
+    {:ok, _view, _html} = live(conn)
   end
 end
