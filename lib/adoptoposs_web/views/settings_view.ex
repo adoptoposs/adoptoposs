a5458c8bdebaadacf73dd60e6d5def914fe54a38
Paul Götze <paul.christoph.goetze@gmail.com>
2020-03-07 01:47:54 +0100

    Allow editing notification settings

--- lib/adoptoposs_web/views/settings_view.ex
+++ lib/adoptoposs_web/views/settings_view.ex
@@ -1,3 +1,10 @@
 defmodule AdoptopossWeb.SettingsView do
   use AdoptopossWeb, :view
+
+  @doc """
+  Returns whether the key in settings currently has the given value.
+  """
+  def active?(%Ecto.Changeset{data: data}, key, value) do
+    Map.get(data.settings, key) == value
+  end
 end
---
+++ test/adoptoposs_web/views/settings_view_test.exs
@@ -0,0 +1,31 @@
+defmodule AdoptopossWeb.SettingsViewTest do
+  use AdoptopossWeb.ConnCase, async: true
+
+  import Adoptoposs.Factory
+
+  alias AdoptopossWeb.SettingsView
+
+  setup do
+    value = "off"
+    key = :email_when_contacted
+
+    changeset =
+      :user
+      |> build(settings: %{key => value})
+      |> Ecto.Changeset.cast(%{}, [])
+      |> Ecto.Changeset.cast_embed(:settings)
+
+    {:ok, changeset: changeset, key: key, value: value}
+  end
+
+  test "active?/3 returns true if the given key/value pair exits in the changeset",
+    %{changeset: changeset, key: key, value: value} do
+    assert SettingsView.active?(changeset, key, value)
+  end
+
+  test "active?/3 returns false if the given key/value pair does not exits in the changeset",
+    %{changeset: changeset, key: key, value: value} do
+    refute SettingsView.active?(changeset, key, "other" <> value)
+    refute SettingsView.active?(changeset, :"other_#{key}", value)
+  end
+end
