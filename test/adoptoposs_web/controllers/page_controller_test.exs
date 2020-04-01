346261267e4302e858aa5f71ee7bb41ebd51065e
Paul Götze <paul.christoph.goetze@gmail.com>
2020-04-01 22:58:23 +0200

    Fix code formatting by running 'mix format'

--- test/adoptoposs_web/controllers/page_controller_test.exs
+++ test/adoptoposs_web/controllers/page_controller_test.exs
@@ -13,7 +13,8 @@ defmodule AdoptopossWeb.PageControllerTest do
 
   @tag login_as: "user123"
   test "page paths are visible for logged in users", %{conn: conn} do
-    [:faq, :privacy] |> Enum.each(fn action ->
+    [:faq, :privacy]
+    |> Enum.each(fn action ->
       conn = get(conn, Routes.page_path(conn, action))
       assert html_response(conn, 200)
     end)
