8c8013a31e361fed426a7b15f086c0d06e2758b9
Paul Götze <paul.christoph.goetze@gmail.com>
2020-04-28 00:07:20 +0200

    Update phoenix to v1.5.1
    See https://github.com/phoenixframework/phoenix/blob/v1.5/CHANGELOG.md

--- mix.exs
+++ mix.exs
@@ -35,8 +35,8 @@ defmodule Adoptoposs.MixProject do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
-      {:phoenix, "~> 1.4.16"},
-      {:phoenix_pubsub, "~> 1.1"},
+      {:phoenix, "~> 1.5.1"},
+      {:phoenix_pubsub, "~> 2.0"},
       {:phoenix_ecto, "~> 4.1"},
       {:ecto_sql, "~> 3.3"},
       {:postgrex, ">= 0.0.0"},
