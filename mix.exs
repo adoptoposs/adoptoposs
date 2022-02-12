b740c4e27248ff8135b0497d8393dbd0eb54e695
Paul Götze <paul.christoph.goetze@gmail.com>
2022-02-12 14:34:39 +0100

    Remove not anymore needed :phoenix compiler with v1.6
    And set minimum required Elixir to v1.11.
    See https://github.com/phoenixframework/phoenix/blob/v1.6/CHANGELOG.md#deprecations

--- mix.exs
+++ mix.exs
@@ -5,9 +5,9 @@ defmodule Adoptoposs.MixProject do
     [
       app: :adoptoposs,
       version: "0.1.0",
-      elixir: "~> 1.5",
+      elixir: "~> 1.11",
       elixirc_paths: elixirc_paths(Mix.env()),
-      compilers: [:phoenix, :gettext] ++ Mix.compilers(),
+      compilers: [:gettext] ++ Mix.compilers(),
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
