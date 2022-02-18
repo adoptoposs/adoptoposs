89f42c4692e7a2bab5679b2e6cbbb9f8bb22dd27
Paul Götze <paul.christoph.goetze@gmail.com>
2022-02-18 15:24:03 +0100

    Bump phoenix_live_view to v0.17
    ...and fix deprecated live_component calls in heex templates.

--- mix.exs
+++ mix.exs
@@ -41,7 +41,7 @@ defmodule Adoptoposs.MixProject do
       {:postgrex, ">= 0.0.0"},
       {:phoenix_html, "~> 3.0"},
       {:phoenix_live_reload, "~> 1.3", only: :dev},
-      {:phoenix_live_view, "~> 0.16"},
+      {:phoenix_live_view, "~> 0.17"},
       {:floki, ">= 0.30.0", only: :test},
       {:phoenix_live_dashboard, "~> 0.6"},
       {:telemetry_metrics, "~> 0.6"},
