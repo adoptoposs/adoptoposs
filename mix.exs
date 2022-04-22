581eb6e440105703ad8032221550bf042fb9e2cf
Paul Götze <paul.christoph.goetze@gmail.com>
2022-04-22 15:33:02 +0200

    Update mjml package to v1.3.2

--- mix.exs
+++ mix.exs
@@ -61,7 +61,7 @@ defmodule Adoptoposs.MixProject do
       {:basic_auth, "~> 2.2.5"},
       {:yaml_elixir, "~> 2.6"},
       {:bamboo, "~> 1.7"},
-      {:mjml, "~> 1.3.0"},
+      {:mjml, "~> 1.3.2"},
       {:navigation_history, "~> 0.4"},
       {:quantum, "~> 3.4.0"},
       {:ecto_enum, "~> 1.4"},
